a0120b60834e52c276119402a7c92d78269a5e8e
HpxAD <companyhpx@gmail.com>
2026-09-20 21:59:24 +0530

    Delete .github/workflows/faceless-shorts-factory-template directory

--- .github/workflows/faceless-shorts-factory-template/voice.py
+++
@@ -1,6 +0,0 @@
-import asyncio, edge_tts
-text=open("script.txt",encoding="utf-8").read()
-async def main():
-    c=edge_tts.Communicate(text=text,voice="en-US-AvaMultilingualNeural")
-    await c.save("script.mp3")
-asyncio.run(main())
